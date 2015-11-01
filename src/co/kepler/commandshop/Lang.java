feed87c7e6e1ff197b869a3601d13f877953093d
BenWoodworth <ben@kepler.co>
2015-11-01 16:51:03 -0500

    First Commit

---
+++ src/co/kepler/commandshop/Lang.java
@@ -0,0 +1,8 @@
+package co.kepler.commandshop;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Lang {
+	public static final String
+	MUST_BE_PLAYER = ChatColor.RED + "You must be a player to use this command!";
+}
